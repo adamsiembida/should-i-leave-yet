e6be9e4ecda064cf556e11b47f0b802e9817f185
Adam Siembida <adam@adamsiembida.com>
2017-08-14 23:56:16 -0400

    add support for desktop notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 import { BingMapComponent } from './bing-map/bing-map.component';
 import { RefreshIndicatorComponent } from './refresh-indicator/refresh-indicator.component';
+
+declare var Notification: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   @ViewChild(BingMapComponent)
   private map: BingMapComponent;
@@ -22,6 +24,14 @@ export class AppComponent {
   private travelTime: number;
   private timer;
   private directionsValid = false;
+  private notificationsSupported = false;
+
+  ngAfterViewInit() {
+    if (('Notification' in window)) {
+      this.notificationsSupported = true;
+      Notification.requestPermission();
+    }
+  }
 
   // Threshold is always valid, this will only updated it when the user makes a change.
   onThresholdChanged(threshold: number) {
@@ -56,6 +66,13 @@ export class AppComponent {
   }
 
   notifyUserToLeave() {
-    alert('Time to leave!');
+    const message = 'Time to leave!';
+
+    // Show a message if supported and enabled, otherwise show an alert.
+    if (!this.notificationsSupported || Notification.permission === 'denied') {
+      alert(message);
+    } else {
+      const notification = new Notification(message);
+    }
   }
 }
