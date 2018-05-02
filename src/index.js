9e3c67672366d389332d113028a4848cdb970efa
Adam Siembida <public@adamsiembida.com>
2018-05-01 21:53:58 -0400

    add bootstrap
    Use precompiled css and js for bootstrap for now.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from "jquery";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip()
+// })
+//
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// })
