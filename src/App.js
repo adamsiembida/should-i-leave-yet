46ff079c26bed4c4cc9a5944d8fdab6040e3be5a
Adam Siembida <public@adamsiembida.com>
2018-05-02 22:11:51 -0400

    add the ThresholdEntry component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 
 import Title from './components/Title'
 import Blurb from './components/Blurb'
+import ThresholdEntry from './components/ThresholdEntry'
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hours: '0',
+      minutes: '45'
+    }
+  }
+
+  handleMinutesChange(e) {
+    const regex = /^[0-5]?[0-9]$/;
+    if (e.target.value === '') {
+      this.setState({minutes: 0});
+    }
+    else if (regex.test(e.target.value)) {
+      this.setState({minutes: e.target.value});
+    }
+  }
+
+  handleHoursChange(e) {
+    const regex = /^[0-9]?[0-9]$/;
+    if (e.target.value === '') {
+      this.setState({hours: 0});
+    }
+    else if (regex.test(e.target.value)) {
+      this.setState({hours: e.target.value});
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +42,13 @@ class App extends Component {
           <Title/>
           <Blurb/>
         </div>
+
+        <ThresholdEntry
+          hours={this.state.hours}
+          minutes={this.state.minutes}
+          onHoursChange={(e) => this.handleHoursChange(e)}
+          onMinutesChange={(e) => this.handleMinutesChange(e)} />
+
       </div>
     );
   }
---
+++ src/components/ThresholdEntry.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './../styles/ThresholdEntry.css'
+
+export default class ThresholdEntry extends Component {
+  render() {
+    return (
+      <div className="row justify-content-center no-gutters">
+
+        {/*Hours*/}
+        <div className="col threshold-entry__input-col">
+          <input className="threshold-entry__input-hours" value={this.props.hours} onChange={(e) => {this.props.onHoursChange(e)}}/>
+        </div>
+        <div className="col col-md-auto label-col">
+          <label className="threshold-entry__label-hours">hours</label>
+        </div>
+
+        {/*Line break when smaller than md*/}
+        <div className="w-100 d-md-none"></div>
+
+        {/*Minutes*/}
+        <div className="col col-md-auto threshold-entry__input-col">
+          <input className="threshold-entry__input-minutes" value={this.props.minutes} onChange={(e) => {this.props.onMinutesChange(e)}}/>
+        </div>
+        <div className="col threshold-entry__label-col">
+          <label className="threshold-entry__label-minutes">minutes</label>
+        </div>
+
+      </div>
+    );
+  }
+};
